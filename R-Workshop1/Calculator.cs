68ed1765deea5dcb8d8044dc2486b493ee43fb14
Michal Dobrzycki <dobrzycki@gmail.com>
2022-08-03 14:38:04 +0200

    Add project files.

---
+++ R-Workshop1/Calculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace R_Workshop1
+{
+    internal class Calculator
+    {
+        private List<string> Operations { get; set; }
+
+        public Calculator()
+        {
+            this.Operations = new List<string>();
+        }
+
+        public double add(double a, double b)
+        {
+            addOperation($"Added {a} and {b} got result: {a+b}");
+            return a + b;
+        }
+
+        private void addOperation(string v)
+        {
+            this.Operations.Add(v);
+        }
+
+        private void clearOperations()
+        {
+            this.Operations = new List<string>();
+        }
+    }
+}
---
+++ R-Workshop1Test/CalculatorTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace R_Workshop1Test
+{
+    public class CalculatorTest
+    {
+        [Test]
+        public void ShouldSumTwoPositiveNumbers()
+        {
+            
+        }
+
+    }
+}
