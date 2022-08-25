4c84fd85d0716001c0df415834f6a2b293583864
Michal Dobrzycki <dobrzycki@gmail.com>
2022-08-25 09:24:36 +0200

    Test double

--- R-Workshop1Test/CalculatorTest.cs
+++ R-Workshop1Test/CalculatorTest.cs
@@ -12,8 +12,11 @@ namespace R_Workshop1Test
             var calc = new Calculator();
             var result = calc.add(5, 6);
             Assert.That(result, Is.EqualTo(11));
-            
-            var expectedOperations = new List<string> { "Added 5 and 6 got result: 11" };
+
+            var otherResult = calc.add(0.1, 0.2);
+            Assert.AreEqual(otherResult, 0.3);
+
+            var expectedOperations = new List<string> { "Added 5 and 6 got result: 11", "Added 0,1 and 0,2 got result: 0,3" };
             CollectionAssert.AreEqual(expectedOperations, calc.Operations);
 
             calc.clearOperations();
